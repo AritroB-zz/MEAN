0dfe61f4a8fd33f02b8c8cdeac46eae62f487d52
AritroB <abasuusa2010@gmail.com>
2020-04-25 21:35:02 -0500

    Created accordion with ngFor

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -6,9 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent {
-  posts = [
-    {title: 'First Post', content: 'This is the first post\'s content'},
-    {title: 'Second Post', content: 'This is the second post\'s content'},
-    {title: 'Third Post', content: 'This is the third post\'s content'},
-  ]
+  // posts = [
+  //   {title: 'First Post', content: 'This is the first post\'s content'},
+  //   {title: 'Second Post', content: 'This is the second post\'s content'},
+  //   {title: 'Third Post', content: 'This is the third post\'s content'},
+  // ];
 }
