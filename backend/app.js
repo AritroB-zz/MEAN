a277dcb327dd3bc173c6af359d88ab99cefe41a8
AritroB <abasuusa2010@gmail.com>
2020-04-30 18:24:16 -0500

    Connected express to node

--- backend/app.js
+++ backend/app.js
@@ -2,3 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('First middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send('Hello there! - express');
+});
+
+module.exports = app;
+
