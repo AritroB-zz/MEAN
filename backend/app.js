e70c3c8d89fac027c46db12a4f3d8040adf3ffee
AritroB <abasuusa2010@gmail.com>
2020-05-02 18:46:18 -0500

    added POST backend point

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -9,6 +13,14 @@ app.use((req, res, next) => {
   );
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS, PUT');
   next();
+});
+
+app.post('/api/posts', (req,res, next) => {
+  const posts = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
 });
 
 app.use('/api/posts', (req, res, next) => {
