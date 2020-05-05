7a65964c8c1fff75b77c6a2ded3d69f14908088b
AritroB <abasuusa2010@gmail.com>
2020-05-04 22:46:25 -0500

    Added mongoose

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS, PUT');
   next();
 });
-
+//vws5KAQ7g8DeKyYl
 app.post('/api/posts', (req,res, next) => {
   const posts = req.body;
   console.log(posts);
