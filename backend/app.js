7d3372fd804bde62710fb3f5768808e464f2acc9
AritroB <abasuusa2010@gmail.com>
2020-05-14 19:33:05 -0500

    Fetched data from database

--- backend/app.js
+++ backend/app.js
@@ -39,23 +39,13 @@ app.post('/api/posts', (req,res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'asdfasd',
-      title: 'First server-side post',
-      content: 'This comes from the server...'
-    },
-    {
-      id: 'kajsldnf123',
-      title: 'Second server-side post',
-      content: 'This comes from the server!'
-    }
+  Post.find().then(documents => {
+      res.status(200).json({
+        message: 'Posts fetched succesfully!',
+        posts: documents
+      });
+    });
 
-  ];
-  res.status(200).json({
-    message: 'Posts fetched succesfully!',
-    posts: posts
-  });
 });
 
 module.exports = app;
