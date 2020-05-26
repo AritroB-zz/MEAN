661ed2332e146163e293a2ecd2f1f1afdd263f47
AritroB <abasuusa2010@gmail.com>
2020-05-26 18:46:19 -0400

    Cleaned up backend routing

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
+const postsRoutes = require("./routes/posts");
 
 const app = express();
 
@@ -26,57 +26,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/posts', (req,res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  console.log(post);
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: 'Post added successfully',
-      postId: createdPost._id
-    });
-  });
-});
-
-app.put("/api/posts/:id", (req,res,next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  Post.updateOne({_id: req.params.id}, post).then(result => {
-    console.log(result);
-    res.status(200).json({message: "Update successful!"});
-  });
-});
-
-app.get('/api/posts', (req, res, next) => {
-  Post.find().then(documents => {
-      res.status(200).json({
-        message: 'Posts fetched succesfully!',
-        posts: documents
-      });
-    });
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({message: 'Post not found!'});
-    }
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({_id: req.params.id}).then(result => {
-    console.log(result);
-    res.status(200).json({message: "Post deleted!"});
-  })
-});
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
 
