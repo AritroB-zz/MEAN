9b1b9fae1bb6f65f86bb6bcea88d6b6b4519df2b
AritroB <abasuusa2010@gmail.com>
2020-05-27 19:56:43 -0400

    Switched to Angular Reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule} from '@angular/material/input';
+import { MatCardModule} from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatExpansionModule} from '@angular/material/expansion';
+import { MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { HttpClientModule } from "@angular/common/http";
 
 
@@ -28,7 +28,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,37 +1,59 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core';
 import { AttachSession } from 'protractor/built/driverProviders';
 import { ThrowStmt } from '@angular/compiler';
 
 import { Post } from '../post.model';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  styleUrls: ['./post-create.component.css'],
 })
-
-export class PostCreateComponent implements OnInit{
+export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
-  private mode = 'create';
-  private postId: string;
   post: Post;
   isLoading = false;
+  form: FormGroup;
 
-  constructor(public postsService: PostsService, public route: ActivatedRoute) {}
+
+  private mode = 'create';
+  private postId: string;
+
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      content: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      //image: new FormControl(null, {validators: [Validators.required]})
+    });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
         this.mode = 'edit';
         this.postId = paramMap.get('postId');
         this.isLoading = true;
-        this.postsService.getPost(this.postId).subscribe(postData => {
+        this.postsService.getPost(this.postId).subscribe((postData) => {
           this.isLoading = false;
-          this.post = {id: postData._id, title: postData.title, content: postData.content};
+          this.post = {
+            id: postData._id,
+            title: postData.title,
+            content: postData.content,
+          };
+          this.form.setValue({
+            title: this.post.title,
+            content: this.post.content,
+          });
         });
       } else {
         this.mode = 'create';
@@ -40,16 +62,25 @@ export class PostCreateComponent implements OnInit{
     });
   }
 
-  onSavePost(form: NgForm) {
-    if(form.invalid) {
+  onSavePost() {
+    console.log("form.invalid: " + this.form.invalid);
+    //console.log("error: " + this.form.errors);
+    if (this.form.invalid) {
       return;
     }
     this.isLoading = true;
-    if (this.mode === 'create') {
-      this.postsService.addPosts(form.value.title, form.value.content);
+    console.log("mode: " + this.mode);
+    if (this.mode === "create") {
+      console.log("title: " + this.form.value.title);
+      console.log("content: " + this.form.value.content);
+      this.postsService.addPosts(this.form.value.title, this.form.value.content);
     } else {
-      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+      this.postsService.updatePost(
+        this.postId,
+        this.form.value.title,
+        this.form.value.content
+      );
     }
-    form.resetForm();
+    this.form.reset();
   }
 }
