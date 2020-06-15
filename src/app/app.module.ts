708de9dea09428613ee99bd8e436f15dd932649b
AritroB <abasuusa2010@gmail.com>
2020-06-15 15:48:57 -0400

    added pagination component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,9 @@ import { MatCardModule} from '@angular/material/card';
 import { MatButtonModule} from '@angular/material/button';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatExpansionModule} from '@angular/material/expansion';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { HttpClientModule } from "@angular/common/http";
-
-
-
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
@@ -34,6 +32,7 @@ import { AppRoutingModule } from './app-routing.module';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
+    MatPaginatorModule,
     MatExpansionModule,
     MatProgressSpinnerModule,
     HttpClientModule
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-post-list',
@@ -17,6 +18,9 @@ export class PostListComponent implements OnInit, OnDestroy{
   // ];
  posts: Post[] = [];
  isLoading = false;
+ totalPosts = 10;
+ postsPerPage = 2;
+ pageSizeOptions = [1, 2, 5, 10];
  private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
@@ -29,6 +33,10 @@ export class PostListComponent implements OnInit, OnDestroy{
         this.isLoading = false;
         this.posts = posts;
       });
+  }
+
+  onChangedPage(pageData: PageEvent){
+    console.log(pageData);
   }
 
   onDelete(postId: string) {
