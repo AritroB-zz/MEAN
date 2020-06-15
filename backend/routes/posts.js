71f9ee89eee6e762947a58550eeae267212d02d9
AritroB <abasuusa2010@gmail.com>
2020-06-15 18:26:25 -0400

    worked on backend pagination

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -73,7 +73,15 @@ router.put(
 });
 
 router.get("", (req, res, next) => {
-  Post.find().then(documents => {
+  const pageSize = +req.query.pagesize;
+  const currentPage = +req.query.page;
+  const postQuery = Post.find();
+  if (pageSize && currentPage) {
+    postQuery
+      .skip(pageSize * (currentPage - 1))
+      .limit(pageSize);
+  }
+  postQuery.then(documents => {
       res.status(200).json({
         message: 'Posts fetched succesfully!',
         posts: documents
