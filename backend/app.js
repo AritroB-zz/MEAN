1ac946f1b7434ecaf73c2f8aabd58e43e7397829
AritroB <abasuusa2010@gmail.com>
2020-06-23 19:00:52 -0500

    Created the User Model

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const postsRoutes = require("./routes/posts");
-const userRoutes = require.apply("/routes/user");
+const userRoutes = require("./routes/user");
 
 const app = express();
 
