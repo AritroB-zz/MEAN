5e117a6b3d6155e03fc83fbeac80f3efeeacf94a
AritroB <abasuusa2010@gmail.com>
2020-06-24 15:53:51 -0500

    Started SPA Authentication

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
-const User = require("../models/user")
+const User = require("../models/user");
+const { TimeoutError } = require("rxjs");
+const user = require("../models/user");
 
 const router = express.Router();
 
@@ -23,6 +26,34 @@ router.post("/signup", (req, res, next) => {
         res.status(500).json({
           error:err
         });
+      });
+    });
+});
+
+router.post('/login', (req, res, next) => {
+  User.findOne({ email req.body.email }).then(user => {
+      if (!user) {
+        return res.status(404).json({
+          message: "Auth failed"
+        });
+      }
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(404).json({
+          message: "Auth failed"
+        });
+      }
+      const token = jwt.sign({email: user.email,
+        userId: user._id},
+        'secret_this_should_be_longeeeeeer',
+        {expiresIn: "1h"}
+      );
+    })
+    .catch(err => {
+      return res.status(404).json({
+        message: "Auth failed"
       });
     });
 });
