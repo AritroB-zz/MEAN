d569f7c4e0663d1911d0ef394805161a65eedfd0
AritroB <abasuusa2010@gmail.com>
2020-06-26 16:19:01 -0500

    Improved UI Messages to show authentication status

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -5,6 +5,7 @@ import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
 import { PageEvent } from '@angular/material/paginator';
 import { runInThisContext } from 'vm';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-post-list',
@@ -23,9 +24,11 @@ export class PostListComponent implements OnInit, OnDestroy{
  postsPerPage = 2;
  currentPage = 1;
  pageSizeOptions = [1, 2, 5, 10];
+ userIsAuthenticated = false;
  private postsSub: Subscription;
+ private authStatusSub: Subscription;
 
-  constructor(public postsService: PostsService) {}
+  constructor(public postsService: PostsService, private authService: AuthService) {}
 
   ngOnInit(){
     this.isLoading = true;
@@ -36,6 +39,12 @@ export class PostListComponent implements OnInit, OnDestroy{
         this.totalPosts = postData.postCount;
         this.posts = postData.posts;
       });
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authStatusSub = this.authService
+    .getAuthStatusListener()
+    .subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+    });
   }
 
   onChangedPage(pageData: PageEvent){
@@ -53,6 +62,7 @@ export class PostListComponent implements OnInit, OnDestroy{
 
   ngOnDestroy() {
     this.postsSub.unsubscribe();
+    this.authStatusSub.unsubscribe();
   }
 
 }
