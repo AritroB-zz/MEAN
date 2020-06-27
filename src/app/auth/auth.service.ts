385fe2e7b6a81a8892e0252dafdc7fddf66e08a8
AritroB <abasuusa2010@gmail.com>
2020-06-27 16:42:49 -0500

    reflected token expiration in UI

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,6 +9,7 @@ import { Router } from '@angular/router';
 export class AuthService{
   private isAuthenticated = false;
   private token: string;
+  private tokenTimer: NodeJS.Timer;
   private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient, private router: Router) {}
@@ -35,11 +36,16 @@ export class AuthService{
 
   login(email: string, password: string) {
     const authData: AuthData = {email: email, password: password}
-    this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
+    this.http.post<{token: string, expiresIn: number}>("http://localhost:3000/api/user/login", authData)
       .subscribe(response => {
         const token = response.token;
         this.token = token;
         if (token) {
+          const expiresInDuration = response.expiresIn;
+          this.tokenTimer = setTimeout(() => {
+            this.logout();
+          }, expiresInDuration * 1000);
+          console.log(expiresInDuration);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
           this.router.navigate(['/']);
@@ -52,5 +58,6 @@ export class AuthService{
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
     this.router.navigate(['/']);
+    clearTimeout(this.tokenTimer);
   }
 }
