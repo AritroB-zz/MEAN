8ba841972bc553582e6ead131e721551569f9670
AritroB <abasuusa2010@gmail.com>
2020-07-02 19:02:03 -0500

    Added error dialog

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,9 +1,10 @@
- import { Component } from '@angular/core';
+ import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   templateUrl: './error.component.html'
 })
 
 export class ErrorComponent {
-  message = 'An unknown error occured!'
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {message: string}) {}
 }
